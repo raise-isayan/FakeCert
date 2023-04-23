5d180b60628fcd5f367822bddb6198e79338301e
isayan <raise.isayan@gmail.com>
2023-04-23 12:33:46 +0900

    Legacy API last release

--- FakeBurpCert/src/test/java/fake/cert/FakeBurpCertTest.java
+++ FakeBurpCert/src/test/java/fake/cert/FakeBurpCertTest.java
@@ -1,11 +1,11 @@
 package fake.cert;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  *
@@ -16,25 +16,29 @@ public class FakeBurpCertTest {
     public FakeBurpCertTest() {
     }
 
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
     }
 
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() {
     }
 
-    @Before
+    @BeforeEach
     public void setUp() {
     }
 
-    @After
+    @AfterEach
     public void tearDown() {
     }
 
 
 /******************************************************************************/
 
+/**
+ *
+ * @return
+ */
 public static java.util.Map createMap() {
     java.util.Map map = new java.util.LinkedHashMap();
     java.io.BufferedReader reader = null;
